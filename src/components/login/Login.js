16e392e12437a67e85113268c01dcdde0f2c5d0c
Noal Brown <noalbrown1@gmail.com>
2020-08-25 15:41:13 -0500

    pp

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,43 +1,150 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { getUser } from '../../redux/reducer';
-import { connect } from 'react-redux';
+import { connect, useStore } from 'react-redux';
 
 const Login = (props) => {
   const [toggle, setToggle] = useState(true);
+  const [userNameInput, setUserName] = useState('');
   const [emailInput, setEmail] = useState('');
+  const [firstNameInput, setFirstName] = useState('');
+  const [lastNameInput, setLastName] = useState('');
   const [passwordInput, setPassword] = useState('');
+
+  const handleUserNameInput = (event) => {
+    const { value } = event.target;
+    setUserName(value);
+  };
+
+  const handleEmailInput = (event) => {
+    const { value } = event.target;
+    setEmail(value);
+  };
+
+  const handleFirstNameInput = (event) => {
+    const { value } = event.target;
+    setFirstName(value);
+  };
+
+  const handleLastNameInput = (event) => {
+    const { value } = event.target;
+    setLastName(value);
+  };
+
+  const handlePasswordInput = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const login = () => {
+    axios
+      .post("/auth/login", {
+        email: emailInput,
+        password: passwordInput,
+      })
+      .then((res) => {
+        props.getUser();
+        props.history.push("/home");
+      })
+      .catch((err) => {
+        alert("Email or password incorrect");
+      });
+  };
+
+  const register = () => {
+    axios
+      .post("/auth/register", {
+        user_name: userNameInput,
+        email: emailInput,
+        password: passwordInput,
+        first_name: firstNameInput,
+        last_name: lastNameInput
+      })
+      .then((res) => {
+        props.getUser();
+        props.history.push("/home");
+      })
+      .catch((err) => {
+        alert("Email is already registered");
+      });
+  };
 
   return (
     <div>
       <header>MyGameStash</header>
-      <section>
-        <div>
-          <input name='email' type='text' value='' placeholder='Email' />
-          <input name='password' type='password' value='' placeholder='Password' />
-        </div>
-        <div>
-          <button>LOGIN</button>
-        </div>
-        <div>
-          <button>Create New User</button>
-        </div>
-      </section>
-      :
-      <section>
-        <div>
-          <input name='email' type='text' value='' placeholder='Email' />
-          <input name='password' type='password' value='' placeholder='Password' />
-        </div>
-        <div>
-          <button>LOGIN</button>
-        </div>
-        <div>
-          <button>Create New User</button>
-        </div>
-      </section>
+      {{ toggle } ? (
+        <section>
+          <div>
+            <input
+              name='email'
+              type='text'
+              value={emailInput}
+              placeholder='Email'
+              onChange={handleEmailInput}
+            />
+            <input
+              name='password'
+              type='password'
+              value={passwordInput}
+              placeholder='Password'
+              onChange={handlePasswordInput}
+            />
+          </div>
+          <div>
+            <button onClick={login}>LOGIN</button>
+          </div>
+          <div>
+            <button onClick={() => { setToggle(!toggle) }}>Create New User</button>
+          </div>
+        </section>
+      ) : (
+          <section>
+            <div>
+              <input
+                name='user_name'
+                type='text'
+                value={userNameInput}
+                placeholder='User Name'
+                onChange={handleUserNameInput}
+              />
+              <input
+                name='email'
+                type='text'
+                value={emailInput}
+                placeholder='Email'
+                onChange={handleEmailInput}
+              />
+              <input
+                name='password'
+                type='password'
+                value={passwordInput}
+                placeholder='Password'
+                onChange={handlePasswordInput}
+              />
+              <input
+                name='first_name'
+                type='text'
+                value={firstNameInput}
+                placeholder='First Name'
+                onChange={handleFirstNameInput}
+              />
+              <input
+                name='last_name'
+                type='text'
+                value={lastNameInput}
+                placeholder='Last Name'
+                onChange={handleLastNameInput}
+              />
+            </div>
+            <div>
+              <button onClick={register}>Create Profile</button>
+            </div>
+            <div>
+              <button onClick={() => { setToggle(!toggle) }}>Cancel</button>
+            </div>
+          </section>
+        )}
     </div>
   )
 }
 
-export default Login;
+export default connect(null, { getUser }(Login));
