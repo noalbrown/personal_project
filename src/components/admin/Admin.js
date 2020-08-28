0e335a82061565425829d8d2c3ae5ce3388bbaee
Noal Brown <noalbrown1@gmail.com>
2020-08-27 21:47:55 -0500

    pp

---
+++ src/components/admin/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './admin.css'
+
+const Admin = (props) => {
+  return (
+    <div className='admin-container'>
+      <h1>Admin</h1>
+    </div>
+  )
+}
+
+export default Admin;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Login from './components/login/Login';
 import Home from './components/home/Home';
 import User from './components/user/User';
 import Contact from './components/contact/Contact'
+import Admin from './components/admin/Admin'
 
 export default (
   <Switch>
@@ -11,5 +12,6 @@ export default (
     <Route path='/home' component={Home} />
     <Route path='/user' component={User} />
     <Route path='/contact' component={Contact} />
+    <Route path='/admin' component={Admin} />
   </Switch>
 );
