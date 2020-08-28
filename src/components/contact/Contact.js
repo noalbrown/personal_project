70ce2bbc231f3ef76b70955f1871c6b34cc42a19
Noal Brown <noalbrown1@gmail.com>
2020-08-27 22:00:34 -0500

    pp

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,7 +4,29 @@ import './contact.css'
 const Contact = (props) => {
   return (
     <div className='contact-container'>
-      <h1>Contact</h1>
+      <section>
+        <h1>Contact Form</h1>
+        <div>
+          <input
+            name='user_name'
+            type='text'
+            value={}
+            placeholder='User name'
+            onChange={}
+          />
+          <input
+            name='email'
+            type='text'
+            value={}
+            placeholder='Email'
+            onChange={}
+          />
+        </div>
+        <form></form>
+        <div>
+          <button>SUBMIT</button>
+        </div>
+      </section>
     </div>
   )
 }
